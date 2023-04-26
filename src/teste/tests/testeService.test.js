ba923c3fda7d750183a13096b0d522f01b94d1cd
muriloepsilva <muriloeduardopenha@gmail.com>
2023-04-25 22:23:38 -0300

    exclusão testes de chamada de banco

---
+++ src/teste/tests/testeService.test.js
@@ -0,0 +1,40 @@
+import { describe, expect, test } from "vitest";
+import TesteService from "../service/testeService";
+import { mandatoryFields } from "../../../utils/constants";
+import TesteRepositoryInMemory from "../repository/testeRepositoryInMemory";
+import { mockUsuario } from "../utils/mock";
+
+const sut = () => {
+  const repository = new TesteRepositoryInMemory();
+  const service = new TesteService(repository);
+
+  return { service };
+};
+
+describe("Testes da classe TesteService", () => {
+  describe("teste", () => {
+    test("Esse tem que lançar uma exceção por não receber o nome", async () => {
+      const { service } = sut();
+
+      await expect(
+        service.teste({ nome: undefined, idade: 18 })
+      ).rejects.toThrow(mandatoryFields("nome"));
+    });
+
+    test("Esse tem que lançar uma exceção por não receber a idade", async () => {
+      const { service } = sut();
+
+      await expect(
+        service.teste({ nome: "Murilo", idade: null })
+      ).rejects.toThrow(mandatoryFields("idade"));
+    });
+
+    test("Esse tem que retornar a frase", async () => {
+      const { service } = sut();
+
+      const response = await service.teste({ nome: "Murilo", idade: 18 });
+
+      expect(response).toEqual("Seu nome é: Murilo e você tem 18 anos");
+    });
+  });
+});
