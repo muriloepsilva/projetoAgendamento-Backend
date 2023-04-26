1999da1c375e3daa96e67381ef706b26a075e849
muriloepsilva <muriloeduardopenha@gmail.com>
2023-04-25 22:08:37 -0300

    novos status code de retorno

--- utils/responsesCodes.js
+++ utils/responsesCodes.js
@@ -1,7 +1,8 @@
 export const ok = (data) => STATUS_CODE_FORMAT(200, data);
-export const noContent = (data) => STATUS_CODE_FORMAT(204, data);
+export const noContent = () => STATUS_CODE_FORMAT(204);
 export const badRequest = (data) => STATUS_CODE_FORMAT(400, data);
 export const notFound = (data) => STATUS_CODE_FORMAT(404, data);
+export const created = () => STATUS_CODE_FORMAT(201);
 
 export const STATUS_CODE_FORMAT = (code, data) => ({
   code,
