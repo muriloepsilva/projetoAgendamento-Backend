b9a091ae93d3c2472a7372310dfc690a6f3852da
muriloepsilva <muriloeduardopenha@gmail.com>
2023-04-25 22:07:56 -0300

    novas rotas de usuarios

--- infra/routes.js
+++ infra/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { testeRouter } from "../src/teste/routes.js";
+import { usuariosRouter } from "../src/usuarios/routes.js";
 
 const router = Router();
 
 router.use("/teste", testeRouter);
+router.use("/usuarios", usuariosRouter);
 
 export { router };
---
+++ src/usuarios/routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import UsuariosController from "./controller/usuariosController.js";
+import adaptRoute from "../../infra/adaptRoute.js";
+
+const usuariosRouter = Router();
+const usuariosController = new UsuariosController();
+
+usuariosRouter.post(
+  "/inserir",
+  adaptRoute.expressRoute(usuariosController, "insertUsuario")
+);
+
+usuariosRouter.get(
+  "/getById/:id",
+  adaptRoute.expressRoute(usuariosController, "listUserById")
+);
+
+usuariosRouter.get(
+  "/allUsers",
+  adaptRoute.expressRoute(usuariosController, "listUsers")
+);
+
+export { usuariosRouter };
